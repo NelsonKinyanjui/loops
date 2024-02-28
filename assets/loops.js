7b181d73967b761ccc8d295e8861ee58379d3591
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-02-28 21:03:25 +0300

    add filter fn

--- assets/loops.js
+++ assets/loops.js
@@ -1,7 +1,10 @@
-function toUpper(string){
-    return string.toUpperCase();
-}
-const cats = ["Lion","Elephant","Tiger","Bufallo"];
 
-const cat = cats.map(toUpper);
-console.log(cat);
+
+function lCat(cats){
+    return  cats.startsWith("L");
+}
+const cats = ["Lion","Elephant","Lizard","Bufallo"];
+
+const filtered = cats.filter(lCat);
+
+console.log(filtered);
